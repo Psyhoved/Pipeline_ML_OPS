05c0fabe9d7f35a949849d6f1ec46fcfaa8a7305
Alexandr Lebedev <mr.alexandrlebedev@mail.ru>
2022-06-23 16:04:12 +0300

    add links to download dataset's in test.py

--- test_model_preprocessing.py
+++ test_model_preprocessing.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 def test_cut_num_split():
-    train = pd.read_csv('projects/SF_regression/data_sets/data_sets/Train.csv')
-    test = pd.read_csv(r'projects/SF_regression/data_sets/data_sets/Test.csv')
+    train = pd.read_csv('https://www.dropbox.com/s/o1jmfe26ri31k3l/Train.csv?dl=0')
+    test = pd.read_csv(r'https://www.dropbox.com/s/qyajy9n51foaa69/Test.csv?dl=0')
 
     data = pd.concat([train, test])
 
